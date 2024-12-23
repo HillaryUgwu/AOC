5ead71966b57b164e90a4662576e9664fa5f478c
hillary.ugwu <hillary.ugwu@cancom.de>
2024-12-23 15:42:58 +0100

    day two problem 2 solved

--- 2024/day_2.py
+++ 2024/day_2.py
@@ -1,24 +1,34 @@
 FILENAME = "raw/2024/day_2.txt"
 
 
-def get_total_distance(loc_id):
-    return sum(abs(a - b) for a, b in loc_id)
+def check_increasing_or_decreasing(replst, n, new_replst=None, flag=False):
+    new_replst = replst if not new_replst else new_replst
+    increasing = all(
+        (replst[i] - replst[i - 1]) in [1, 2, 3] for i in range(1, len(replst))
+    )
+    decreasing = all(
+        (replst[i - 1] - replst[i]) in [1, 2, 3] for i in range(1, len(replst))
+    )
 
+    if increasing or decreasing:
+        return 1
 
-def check_increasing_or_decreasing(lst, flag=False):
-    if all((lst[i] - lst[i - 1]) in [1, 2, 3] for i in range(1, len(lst))):
-        return 1
-    elif all((lst[i - 1] - lst[i]) in [1, 2, 3] for i in range(1, len(lst))):
-        return 1
-    if not flag:
+    if not flag or (n >= len(new_replst)):
         return 0
-    check_increasing_or_decreasing(lst)
+
+    reptup = tuple(new_replst)
+    while True:
+        n += 1
+        reptuplst = list(reptup)
+        reptuplst.pop(len(reptup) - n)
+        return check_increasing_or_decreasing(reptuplst, n, new_replst, flag=True)
 
 
 def count_safe_reports(reports, flag=False):
     safe_reps = 0
-    for lines in reports:
-        reps = check_increasing_or_decreasing(lines, flag)
+    for i, lines in enumerate(reports):
+        n = 0
+        reps = check_increasing_or_decreasing(lines, n, flag=flag)
         safe_reps += reps
     return safe_reps
 
@@ -36,9 +46,9 @@ def get_location_id(FILENAME):
 def main():
     reports = get_location_id(FILENAME)
     safe_reps = count_safe_reports(reports)
-    # damp = count_safe_reports(reports, True)
+    damp = count_safe_reports(reports, flag=True)
     print(f"The total number of safe reports: {safe_reps}")  # Question 1
-    # print(f"The Problem Dampener number of safe reports: {damp}")  # Question 2
+    print(f"The Problem Dampener number of safe reports: {damp}")  # Question 2
 
     print("done")
 
