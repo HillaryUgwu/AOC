7c9e348c1787a8272076aad7accb52ca9d29bf3e
hillary.ugwu <hillary.ugwu@cancom.de>
2024-12-23 01:13:47 +0100

    day one question 2 solved

--- 2024/day_1.py
+++ 2024/day_1.py
@@ -5,6 +5,17 @@ def get_total_distance(loc_id):
     return sum(abs(a - b) for a, b in loc_id)
 
 
+def get_similarity_score(loc_id1, loc_id2):
+    sim_score = 0
+    for id1 in loc_id1:
+        count = 0
+        for id2 in loc_id2:
+            if id1 == id2:
+                count += 1
+        sim_score += id1 * count
+    return sim_score
+
+
 def get_location_id(FILENAME):
     loc1, loc2 = [], []
     with open(FILENAME) as f:
@@ -21,9 +32,13 @@ def main():
     loc_id1.sort()
     loc_id2.sort()
     t_dist = get_total_distance(zip(loc_id1, loc_id2))
+    sim_score = get_similarity_score(loc_id1, loc_id2)
     print(
         f"The total distance between the left list and the right list is {t_dist}"
     )  # Question 1
+    print(
+        f"The similarity score between the left list and the right list is {sim_score}"
+    )  # Question 2
 
     print("done")
 
